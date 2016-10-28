e8b7789cd87dad7296b5b48b1f6b160e3b2134cc
Yoannis Jamar <yoannis.j@gmail.com>
2016-10-28 03:43:02 +0200

    Improve docs in readme + cleanup

--- src/util/chain.js
+++
@@ -1,9 +0,0 @@
-'use-strict';
-
-var compose = require('src/util/compose');
-
-module.exports = function(proto, props, options) {
-  var res = Object.create(proto);
-  // if own properties are passed, use composition to include them
-  return (props ? compose(res, props, options) : res);
-};
